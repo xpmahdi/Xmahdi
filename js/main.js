845a954663507c6ba748e2b7ddadad31dbf0072b
xpmahdi <mahdidev1892@gmail.com>
2025-03-16 23:06:53 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { typeWrite } from "./typeWrite.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
